47205b81a1342e30d399065deee96ffb7c4e963d
Jungwon_Park <Jungwon.Park@edu.sait.ca>
2024-04-01 18:11:48 -0600

    Add node

--- Assignment3/Utility/Node.cs
+++ Assignment3/Utility/Node.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace Assignment3.Utility
 {
-    
     public class Node
     {
-  
+        private object element;
+     private Node next;
+
+     public Node(object o)
+     {
+         this.Element = o;
+     }
+
+     public Node(object o, Node n)
+     {
+         this.Element = o;
+         this.Next = n;
+     }
+
+     public object Element { get => element; set => element = value; }
+     public Node Next { get => next; set => next = value; }
     }
     
 }
